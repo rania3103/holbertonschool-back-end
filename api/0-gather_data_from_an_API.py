79ac950a7c3208494f80bb008471df26a69be9ee
rania3103 <raniaabassi33@gmail.com>
2023-12-19 11:50:05 +0100

    code should not be executed when imported

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,17 +4,18 @@ for a given employee ID, returns information about
 his/her TODO list progress."""
 import requests
 from sys import argv
-employee_id = argv[1]
-req_name = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
-name = req_name["name"]
-req_todo = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)).json()
-completed_tasks = 0
-for i in req_todo:
-    if i["completed"]:
-        completed_tasks += 1
-print("Employee {} is done with tasks({}/{}):".format(name,
-      completed_tasks, len(req_todo)))
-for i in req_todo:
-    print("  {}".format(i["title"]))
+if __name__ == "__main__":
+    employee_id = argv[1]
+    req_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
+    name = req_name["name"]
+    req_todo = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)).json()
+    completed_tasks = 0
+    for i in req_todo:
+        if i["completed"]:
+            completed_tasks += 1
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          completed_tasks, len(req_todo)))
+    for i in req_todo:
+        print("  {}".format(i["title"]))
